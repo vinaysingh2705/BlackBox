00f1cc07a499ee6cd23f02c63c14fcabf808695f
vinaysingh2705 <vinay.singh@clazar.io>
2022-12-18 22:05:31 +0530

    updated somethong

--- backend/BlackBox/settings.py
+++ backend/BlackBox/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
- 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +75,20 @@ WSGI_APPLICATION = 'BlackBox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blackbox',
+        'USER': 'vinay',
+        'PASSWORD': "123456",
+        'HOST': 'localhost',
+        'PORT': "5432",
     }
 }
 
