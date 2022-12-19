e1af09c0451c04e4a2bf054b24779e4e93a2ca30
vinaysingh2705 <vinay.singh@clazar.io>
2022-12-19 14:50:49 +0530

    updte

--- backend/BlackBox/settings/dev.py
+++ backend/BlackBox/settings/dev.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'BlackBox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blackbox',
-        'USER': 'vinay',
-        'PASSWORD': "123456",
-        'HOST': 'localhost',
-        'PORT': "5432",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'blackbox',
+#         'USER': 'vinay',
+#         'PASSWORD': "123456",
+#         'HOST': 'localhost',
+#         'PORT': "5432",
+#     }
+# }
 
 
 # Password validation
