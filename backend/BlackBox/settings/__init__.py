793f5eb3d4efa753054fd68f7f07a32c87b4e971
vinaysingh2705 <vinay.singh@clazar.io>
2023-01-20 15:19:05 +0530

    update

--- backend/BlackBox/settings/__init__.py
+++ backend/BlackBox/settings/__init__.py
@@ -4,11 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-print("========================================")
-print("========================================")
-print(os.environ.get('DJANGO_DEVELOPMENT'))
-print("========================================")
-print("========================================")
 if os.environ.get('DJANGO_DEVELOPMENT', 'False') == 'True':
     from .dev import *
 else:
