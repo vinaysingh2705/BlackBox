8904a731a5cf25391166c42796a994dbdbd18ee0
vinaysingh2705 <vinay.singh@clazar.io>
2023-01-20 15:46:36 +0530

    test

---
+++ backend/vinay/tests/test_second.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+import time
+
+
+class TestSomething(TestCase):
+    
+    def test_name(self):
+        time.sleep(5)
+        self.assertEqual(1,1)
+    def test_name1(self):
+        time.sleep(1)
+        self.assertEqual(1,1)
+    def test_name2(self):
+        time.sleep(2)
+        self.assertEqual(1,1)
